7f2d051b0cf687241fa865990bf78bf6244929bb
cepeasbhy <asbhycepe01@gmail.com>
2020-12-11 19:48:25 +0800

    Created an option for user and inputs

--- monthgenerator.java
+++ monthgenerator.java
@@ -14,6 +14,30 @@ public class monthgenerator {
 		
 		boolean leap_year = leap(year);
 		
+		System.out.println("Select a month: ");
+		System.out.println("-----------------------------");
+		System.out.println("[1] JANUARY     [7] JULY");
+		System.out.println("[2] FEBRUARY    [8] AUGUST");
+		System.out.println("[3] MARCH       [9] SEPTEMBER");
+		System.out.println("[4] APRIL       [10] OCTOBER");
+		System.out.println("[5] MAY         [11] NOVEMBER");
+		System.out.println("[6] JUNE        [7] DECEMBER");
+		
+		System.out.print("   -Enter your choice: ");
+		int month = input.nextInt();
+		
+		System.out.println("Select a day: ");
+		System.out.println("-----------------------------");
+		System.out.println("[1] SUNDAY     [5] THURSDAY");
+		System.out.println("[2] MONDAY     [6] FRIDAY");
+		System.out.println("[3] TUESDAY    [7] SATURDAY");
+		System.out.println("[4] WEDNESDAY");
+		
+		System.out.print("   -Enter your choice: ");
+		int day = input.nextInt();
+		
+		
+		
 	}
 	
 	public static boolean leap(int year) {
